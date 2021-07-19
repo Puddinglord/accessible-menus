73df7404fefea6039769fe84e67b60f2eceb75e9
Dakotah <Puddinglord3@gmail.com>
2021-07-18 21:09:55 -0400

    Made it respect the alt key

--- main.js
+++ main.js
@@ -5,33 +5,35 @@ Hooks.on("ready", function () {
 });
 
 function keyDown(event) {
-  switch (event.keyCode) {
-    case 67:
-      ui.sidebar.activateTab("chat");
-      break;
-    case 84:
-      ui.sidebar.activateTab("combat");
-      break;
-    case 83:
-      ui.sidebar.activateTab("settings");
-      break;
-    case 65:
-      ui.sidebar.activateTab("actors");
-      break;
-    case 73:
-      ui.sidebar.activateTab("items");
-      break;
-    case 74:
-      ui.sidebar.activateTab("journal");
-      break;
-    case 82:
-      ui.sidebar.activateTab("tables");
-      break;
-    case 77:
-      ui.sidebar.activateTab("playlists");
-      break;
-    case 80:
-      ui.sidebar.activateTab("compendium");
-      break;
+  if (event.altKey) {
+    switch (event.keyCode) {
+      case 67:
+        ui.sidebar.activateTab("chat");
+        break;
+      case 84:
+        ui.sidebar.activateTab("combat");
+        break;
+      case 83:
+        ui.sidebar.activateTab("settings");
+        break;
+      case 65:
+        ui.sidebar.activateTab("actors");
+        break;
+      case 73:
+        ui.sidebar.activateTab("items");
+        break;
+      case 74:
+        ui.sidebar.activateTab("journal");
+        break;
+      case 82:
+        ui.sidebar.activateTab("tables");
+        break;
+      case 77:
+        ui.sidebar.activateTab("playlists");
+        break;
+      case 80:
+        ui.sidebar.activateTab("compendium");
+        break;
+    }
   }
 }
